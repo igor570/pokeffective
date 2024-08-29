676e0c1c670cb37eed3c221150179e0e428e30fa
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2024-08-29 16:15:50 +0100

    Fully functional
    - App displays correct damage to and from for each type
    - App is responsive for mobile devices
    - TODO: Fix grid width for large screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 
 function App() {
   const [selectedType, setSelectedType] = useState<string>("bug");
-  const [data, error] = useFetchType(selectedType);
+  const [data, error, isLoading] = useFetchType(selectedType);
 
   const handleClick = (type: string) => {
     if (type && selectedType) {
@@ -15,14 +15,12 @@ function App() {
 
   if (error) return <div>An error was found.</div>;
 
-  if (!data) return <div>Loading...</div>;
-
   return (
-    <main className="h-screen bg-zinc-900">
-      <h1 className="text-5xl text-center font-bold pt-20 text-white">
+    <main className="min-h-screen bg-zinc-900 pb-10">
+      <h1 className="text-5xl text-center font-bold pt-10 text-white">
         Pokeffective
       </h1>
-      <div className="grid grid-cols-4 grid-rows-3  gap-6 mt-20 p-10 sm:justify-center place-content-center">
+      <div className="grid grid-cols-4 grid-rows-3  gap-6 mt-10 p-10 sm:justify-center place-content-center">
         {pokemonType.map((type, index) => (
           <div
             key={index}
@@ -41,11 +39,37 @@ function App() {
           </div>
         ))}
       </div>
-      <div className="text-white text-center">
-        {selectedType} takes double damage from{" "}
-        {data?.doubleDamageFrom.map((item) => (
-          <div className="text-2xl">{item.name}</div>
-        ))}
+      <div className="flex flex-col gap-y-10">
+        <div className="text-white text-center">
+          {selectedType} <span className="text-green-400">deals</span> double
+          damage <span className="text-green-400">to</span>{" "}
+          <div className="flex flex-row gap-x-5 justify-center">
+            {isLoading ? (
+              <div>🌀</div>
+            ) : (
+              data?.doubleDamageTo.map((item) => (
+                <div key={item.name} className="text-[24px]">
+                  {item.name}
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+        <div className="text-white text-center">
+          {selectedType} <span className="text-red-400">takes</span> double
+          damage <span className="text-red-400">from</span>{" "}
+          <div className="flex flex-row gap-x-5 justify-center">
+            {isLoading ? (
+              <div>🌀</div>
+            ) : (
+              data?.doubleDamageFrom.map((item) => (
+                <div key={item.name} className="text-[24px]">
+                  {item.name}
+                </div>
+              ))
+            )}
+          </div>
+        </div>
       </div>
     </main>
   );
@@ -54,10 +78,10 @@ function App() {
 export default App;
 
 const useFetchType = (type: string) => {
-  const { data, error } = useQuery({
+  const { data, error, isLoading } = useQuery({
     queryKey: ["type", type],
     queryFn: () => fetchPokemonType(type),
   });
 
-  return [data, error] as const;
+  return [data, error, isLoading] as const;
 };
